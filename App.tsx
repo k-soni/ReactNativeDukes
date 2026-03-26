c5868fba11672131e1cbf8d757fa1e3daaf5c04e
Kamal Soni <sonqkama@amazon.com>
2026-03-27 03:53:06 +0530

    Feat: Added boiler plate

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View >
+          <Text style={styles.title} >Open up App.tsx to working on your app!</Text>
+           <Text>Open up App.tsx to working on your app!</Text>
+            <Text>Open up App.tsx to working on your app!
+               <Text>Open up App.tsx to working on your app!</Text>
+              </Text>
+             <Text>Open up App.tsx to working on your app!</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#bc3737',
   },
+  title: {
+    fontSize: 40
+  }
+
+
 });
